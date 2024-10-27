ff4046b41b1a15ff72a8566bf0499ff1c26c8117
Jordi <55429631+jorbush@users.noreply.github.com>
2024-10-27 11:32:52 +0100

    fmt

--- app/components/ParticipantsTable.tsx
+++ app/components/ParticipantsTable.tsx
@@ -152,12 +152,12 @@ export const ParticipantsTable = () => {
                             className="rounded"
                         />
                         Añadir supervisor
-                        <div className="relative group">
+                        <div className="group relative">
                             <IoInformationCircle
-                                className="text-gray-800 dark:text-white hover:text-gray-700 hover:dark:text-neutral-200 cursor-help"
+                                className="cursor-help text-gray-800 hover:text-gray-700 dark:text-white hover:dark:text-neutral-200"
                                 size={15}
                             />
-                            <div className="absolute left-1/2 -translate-x-1/2 bottom-full mb-2 hidden group-hover:block w-48 p-2 bg-neutral-50 dark:bg-gray-800 dark:text-white text-sm rounded-lg shadow-lg">
+                            <div className="absolute bottom-full left-1/2 mb-2 hidden w-48 -translate-x-1/2 rounded-lg bg-neutral-50 p-2 text-sm shadow-lg group-hover:block dark:bg-gray-800 dark:text-white">
                                 Recibirá todas las asignaciones y no participa
                             </div>
                         </div>
